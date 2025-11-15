bb1055fc6ab2b4221655319c5291ad3cf31f81b8
evicno <229501549+evicno@users.noreply.github.com>
2025-11-15 11:53:46 +0100

    Complete playRound function

--- javascript.js
+++ javascript.js
@@ -27,7 +27,7 @@ function playRound(humanChoice, computerChoice) {
             computerScore ++;
             console.log("You lose! Paper beats Rock.");
         }
-        else {
+        else { // computerChoice == "scissors"
             humanScore ++;
             console.log("You win! Rock beats Scissors.");
         }
@@ -37,12 +37,21 @@ function playRound(humanChoice, computerChoice) {
             humanScore ++;
             console.log("You win! Paper beats Rock.");
         }
-        else {
+        else { // computerChoice == "scissors"
             computerScore ++;
             console.log("You lose! Scissors beat Paper.");
         }
     }
-    
+    else { // humanChoice === "scissors"
+        if (computerChoice === "paper") {
+            humanScore ++;
+            console.log("You win! Scissors beat Paper.");
+        }
+        else {  // computerChoice == "rock" 
+            computerScore ++;
+            console.log("You lose! Rock beats Scissors.");
+        }
+}
 }
 
 humanScore = 0;
@@ -54,4 +63,3 @@ console.log(computerSelection);
 playRound(humanSelection, computerSelection);
 console.log(humanScore);
 console.log(computerScore);
-
