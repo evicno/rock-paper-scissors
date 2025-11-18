e06c53ea0c7c4e5a2583063cd853ef9675d89938
evicno <229501549+evicno@users.noreply.github.com>
2025-11-18 17:42:03 +0100

    Add DOM methods to display results of a round Add round_results div to html, change console.log to DOM methods to display result of a round

--- javascript.js
+++ javascript.js
@@ -19,37 +19,38 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    const roundResult = document.querySelector("#round_result");
     if (humanChoice === computerChoice) {
-        console.log("Tie! Play again.");
+        roundResult.textContent = "Tie! Play again.";
     }
     else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
             computerScore ++;
-            console.log("You lose! Paper beats Rock.");
+            roundResult.textContent = "You lose! Paper beats Rock.";
         }
         else { // computerChoice == "scissors"
             humanScore ++;
-            console.log("You win! Rock beats Scissors.");
+            roundResult.textContent = "You win! Rock beats Scissors.";
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             humanScore ++;
-            console.log("You win! Paper beats Rock.");
+            roundResult.textContent = "You win! Paper beats Rock.";
         }
         else { // computerChoice == "scissors"
             computerScore ++;
-            console.log("You lose! Scissors beat Paper.");
+            roundResult.textContent = "You lose! Scissors beat Paper.";
         }
     }
     else { // humanChoice === "scissors"
         if (computerChoice === "paper") {
             humanScore ++;
-            console.log("You win! Scissors beat Paper.");
+            roundResult.textContent = "You win! Scissors beat Paper.";
         }
         else {  // computerChoice == "rock" 
             computerScore ++;
-            console.log("You lose! Rock beats Scissors.");
+            roundResult.textContent = "You lose! Rock beats Scissors.";
         }
 }
 }
